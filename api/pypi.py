d39c157fd50ed56d430184ead6d570b97b5fd213
KarinaBertizini <karinabertizini@gmail.com>
2021-10-28 18:44:42 +0000

    28-10-2021

--- api/pypi.py
+++ api/pypi.py
@@ -1,20 +1,36 @@
 import requests
-
-# Referências sobre o uso do requests:
-#
-# Fazendo requisições:
-# https://docs.python-requests.org/en/master/user/quickstart/#make-a-request
-# Usando JSON retornado:
-# https://docs.python-requests.org/en/master/user/quickstart/#json-response-content
-
+import json
+ 
 def version_exists(package_name, version):
-    # TODO
-    # Fazer requisição na API do PyPI para checar se a versão existe
-    return False
-
-
+   
+     
+#Variável ‘consulta’ vai receber o valor da requisição da API https://pypi.org/pypi/json
+    consulta = requests.get(f"https://pypi.org/pypi/{package_name}/{version}/json")
+     #response = r.json()
+ 
+#O if vai receber o valor  da ‘consulta’ para retornar o código de status.  Se ele retornar
+# 200 quer dizer que o servidor existe, caso contrário não
+    if consulta.status_code == 200:
+        return True
+    else:
+        return False
+ 
+ 
+ 
 def latest_version(package_name):
-    # TODO
-    # Fazer requisição na API do PyPI para descobrir a última versão
-    # de um pacote. Retornar None se o pacote não existir.
-    return "?"
+ 
+ 
+#A request.get chamar a API e salva o valor em ‘achar’
+    achar = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+ 
+#No if ela compara o valor em ‘achar’  com 404 e se ela for igual a 404 irá retornar
+# ‘None’ que está vazio o que irá encerrar o processo
+    if achar.status_code == 404:
+        return None
+ 
+#se ele der diferente ele vai retornar os valores das chaves em uma lista a última versão
+    else:
+        achar = achar.json()
+        return list(achar['releases'].keys())[-1]
+ 
+
